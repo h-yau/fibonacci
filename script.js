c742b60c346b74140db6b11ebad58bf25b7a07b7
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-04-25 10:18:13 -0700

    Implement both iterative and recursive methods

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 // iterative method
 
+// const fibs = (number) => {
+//   if (number <= 1) return [0];
+
+//   if (number == 2) return [0, 1];
+
+//   const ans = [0, 1];
+
+//   for (let i = 3; i <= number; i++) {
+//     ans.push(ans[i - 2] + ans[i - 3]);
+//   }
+//   return ans;
+// };
+
+// recursive method
+
+const fibnum = (number) => {
+  if (number <= 1) return 0;
+  if (number == 2) return 1;
+
+  return fibnum(number - 1) + fibnum(number - 2);
+};
+
 const fibs = (number) => {
   if (number <= 1) return [0];
 
-  if (number == 2) return [0, 1];
+  return fibs(number - 1).concat(fibnum(number));
 };
 
 console.log(fibs(1));
 console.log(fibs(2));
+
+console.log(fibs(8));
